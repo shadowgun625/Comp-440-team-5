8b6f016caac40319293595e4666229ff3ff9cd59
shdowgun625 <shadowgun625@gmail.com>
2020-04-27 02:29:36 -0700

    mergeing branches

--- PHP/welcome.php
+++ PHP/welcome.php
@@ -1,29 +1,11 @@
 <?php
 // Initialize the session
 session_start();
-include 'ChromePhp.php';
-ChromePhp::log('Welcome Page granted');
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
-}
-
-if(isset($_POST['insert_a_blog'])){
-    define('DB_SERVER', 'localhost');
-    define('DB_USERNAME', 'root');
-    define('DB_PASSWORD', '');
-    define('DB_NAME', 'assign3');
-    $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    if ($link->connect_error) {
-        die("Connection failed: " . $link->connect_error);
-    }
-
-
-
-
-$link->close();
 }
 ?>
  
@@ -43,32 +25,15 @@ $link->close();
 
     <div class="page-header">
         <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-  <div class="wrapper">
-        <label>Fill in the fielsds to insert a new blog.</label>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-            <div class="form-group">
-                <label>Subject</label>
-                <input type="text" name="subject" class="form-control" >  
-            </div>    
-            <div class="form-group ">
-                <label>Description</label>
-                <input type="text" name="description" class="form-control" >
-                
-            </div>
-            <div class="form-group">
-                <label>Tag</label>
-                <input type="text" name="tag" class="form-control" >   
-            </div>
-
-            <input type="submit" name="insert_a_blog" class="btn btn-primary" value="Insert a Blog" />
-        </form>
-</div>
-
-        
-        
     </div>
     <p>
         <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+    </p>
+	<p>
+        <a href="initalDB.php" class="btn btn-danger">Initalize database</a>
+    </p>
+	<p>
+        <a href="blog.php" class="btn btn-danger">Insert Blog</a>
     </p>
 </body>
 </html>
