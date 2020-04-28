741a0ed48e5b6fecd7b0bcb47b0c6e6625efc4f4
aram392 <arambalayan1@gmail.com>
2020-04-27 20:35:52 -0700

    stuck on sql statement. not giving result needed

--- PHP/part3-2.php
+++ PHP/part3-2.php
@@ -1,14 +1,30 @@
 <?php
 // Initialize the session
 session_start();
- 
+
+// Include config file
+require_once "config.php";
+
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
+} 
+
+//$sql = "SELECT DISTINCT postuser FROM blogs, blogstags WHERE (blogs.postuser = blogs.postuser AND blogstags.tag = 'Y') AND (blogs.postuser = blogs.postuser AND blogstags.tag = 'X')" ;
+$sql = "SELECT DISTINCT postuser FROM blogs, blogstags WHERE (blogstags.tag = 'Y') AND (blogstags.tag = 'X')";
+
+if ($stmt = $link->prepare($sql)) {
+    $stmt->execute();
+    $stmt->bind_result($postuser,);
+    while ($stmt->fetch()) {
+            printf("%s\n", $postuser);
+    }
+    $stmt->close();
 }
+
 ?>
- 
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -21,22 +37,9 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 </head>
 <body>
-
     <p>
-        <a href="part3-2.php" class="btn btn-danger">Test</a>
+        <a href="welcome.php" class="btn btn-danger">Go back</a>
     </p>
 
-    <div class="page-header">
-        <h1>Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
-    </div>
-    <p>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
-    </p>
-	<p>
-        <a href="initalDB.php" class="btn btn-danger">Initalize database</a>
-    </p>
-	<p>
-        <a href="blog.php" class="btn btn-danger">Insert Blog</a>
-    </p>
 </body>
 </html>
